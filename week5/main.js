75ac6c480635538bad17232a5a8473d5ba3fe04e
s3920396 <s3920396@student.rmit.edu.au>
2025-08-22 10:24:34 +1000

    Update main.js

--- week5/main.js
+++ week5/main.js
@@ -1,5 +1,11 @@
+//variables
+const myName = "Patrick";
+let myHungriness = 0.3;
+
 // console.log() sends message to browser console
 console.log("hello");
 
 let stepNumber = 4;
-console.log("Taking step no:");
+console.log("Taking step:", stepNumber, "... i think");
+
+let name = prompt("What's your name?");
