8af9242a109acf60c3e4e0b3cf17cf1381129818
s3920396 <s3920396@student.rmit.edu.au>
2025-08-22 11:31:03 +1000

    Update main.js

--- weather/main.js
+++ weather/main.js
@@ -1,3 +1,12 @@
 // find my elements
 const appBody = document.querySelector("body");
 const temptInput = document.querySelector("#tempInput");
+
+function interpretTemp() {
+  console.log(temptInput.value);
+  // temp 0 : 10 freezing
+  // temp 10 : 18 cold
+  // temp 18 : 26 warm
+  // temp 26 : 30 warm
+  // temp 30 + : hot
+}
