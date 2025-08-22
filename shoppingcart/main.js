82324a5b708b34b6ae0e458e956effd3e8de3846
s3920396 <s3920396@student.rmit.edu.au>
2025-08-22 12:02:53 +1000

    Update main.js

--- shoppingcart/main.js
+++ shoppingcart/main.js
@@ -1,6 +1,16 @@
+let cartTotal = 0;
+
 let shoppingcart = [
   { name: "t-shirt", price: 20 },
   { name: "jeans", price: 50 },
   { name: "sneakers", price: 80 },
   { name: "backpack", price: 30 },
 ];
+
+function addToCartTotal(item) {
+  console.log("item added:", item.name);
+  console.log("item price:", item.price);
+  cartTotal = cartTotal + item.price;
+  console.log("running total:", total);
+}
+shoppingcart.forEach(addToCartTotal);
