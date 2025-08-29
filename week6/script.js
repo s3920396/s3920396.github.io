d568d47a21a04e6e861d21e4d790be15ece8d804
s3920396 <s3920396@student.rmit.edu.au>
2025-08-29 10:17:16 +1000

    Update script.js

--- week6/script.js
+++ week6/script.js
@@ -1,11 +1,13 @@
 // we can use window to find certain properties
 let windowWidth = window.innerWidth;
-console.log(windowWidth);
+//console.log(windowWidth);
 // find out the url location
-console.log(window.location);
+//console.log(window.location);
 // doumet can be used to find html
-console.log(document.title);
+//console.log(document.title);
 // set the title
 //document.title = "new title";
 // can find the body
 //document.body.style.backgroundColor = "red";
+// use a navigator code to fid more software
+console.log(window.navigator.userAgent);
