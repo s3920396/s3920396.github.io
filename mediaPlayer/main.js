4a5bf48b4f79684e081063d1a197cdbbcaf93b9a
s3920396 <s3920396@student.rmit.edu.au>
2025-09-12 11:11:38 +1000

    text

---
+++ mediaPlayer/main.js
@@ -0,0 +1,7 @@
+/* play pause button behaviour: If media is not playing
+if media is playing
+When I click it begins and pauses the playback of the media file 
+feedback
+toggle icon based on play state
+cursor change on hover
+*/
