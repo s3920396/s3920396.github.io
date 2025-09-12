6c06a40b4eeb23db030c3e3f1d23ed05d769265f
s3920396 <s3920396@student.rmit.edu.au>
2025-09-12 11:29:01 +1000

    adding play and pause

--- mediaPlayer/main.js
+++ mediaPlayer/main.js
@@ -5,3 +5,8 @@ feedback
 toggle icon based on play state
 cursor change on hover
 */
+
+/* time line behaviour
+it should update as media playback occors and to current 
+I should be able to click and jump time
+*/
