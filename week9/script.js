7069580405d1ef26638e3049f3fde62cc46723fa
s3920396 <s3920396@student.rmit.edu.au>
2025-10-03 10:53:49 +1000

    Update script.js

--- week9/script.js
+++ week9/script.js
@@ -1,10 +1,11 @@
 // find our elemenets
-const transformOuter = document.querySelector;(".outer");
-const ball = document.querySelector(.ball);
-const moveButton = document.querySelector("#move-button"); 
-const scaleutton = document.querySelector("#scale-button"); 
-const rotateButton = document.querySelector("#rotate-button"); 
-const resetButton = document.querySelector("#reset-button"); 
+const transformOuter = document.querySelector;
+(".outer");
+const ball = document.querySelector(".ball");
+const moveButton = document.querySelector("#move-button");
+const scaleutton = document.querySelector("#scale-button");
+const rotateButton = document.querySelector("#rotate-button");
+const resetButton = document.querySelector("#reset-button");
 
 //define our transform variables
 let ballTranslateX = 0;
@@ -12,6 +13,27 @@ let ballRotate = 0;
 let ballScale = 1;
 
 //this function will take the current values and apply to ball
-function.updateTransform(){
-    ball.style.transformation = `translateX(${ballTranslateX}) rotate(${ballRotate}) scale(${ballScale})`;
+function updateTransform() {
+  ball.style.transformation = `translateX(${ballTranslateX}px) rotate(${ballRotate}deg) scale(${ballScale})`;
 }
+
+//move ball
+function moveBall() {
+  ballTranslateX += 10;
+  updateTransform();
+}
+// attach to button
+moveButton.addEventListener("click", moveBall);
+
+//rotate ball
+function rotateBall(){
+    ballRotate += 15;
+    updateTransform();
+}
+rotateButton.addEventListener("click", rotateBall);
+
+//scale the ball
+function scaleBall{
+    
+}
+
