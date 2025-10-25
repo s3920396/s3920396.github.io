e06143c5a0d5a00ced30b89f62c2669595b6c31f
s3920396 <s3920396@student.rmit.edu.au>
2025-10-25 12:24:01 +1100

    Update main.js

--- The A3 card game/main.js
+++ The A3 card game/main.js
@@ -4,3 +4,31 @@ var cardList =[
     "queen",
     "jack"
 ]
+
+var cardSet;
+var board= [];
+
+window.onload = function() {
+    shuffleCards();
+    startGame();
+}
+
+function shuffleCards() {
+    cardSet = cardList.concat(cardList); //make two of each card
+    console.log(cardSet);
+    //shuffle the cards
+    for (let i = 0; i <cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //get random variables for the cards
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
+
+
+}
+
+function startGame {
+    
+}
