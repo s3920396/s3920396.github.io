a274259452a2ccbaf1a41be3fc678a9448ce760f
s3920396 <s3920396@student.rmit.edu.au>
2025-10-26 17:28:56 +1100

    adjusting

--- The A3 card game/main.js
+++ The A3 card game/main.js
@@ -1,30 +1,4 @@
 
-const cardArray = [
-    {
-        name: "king",
-        img: "assets/king.png"
-    },
-     {
-        name: "king",
-        img: "assets/king.png"
-    },
-       {
-        name: "queen",
-        img: "assets/queen.png"
-    },
-     {
-        name: "queen",
-        img: "assets/queen.png"
-    },   
-    {
-        name: "jack",
-        img: "assets/jack.png"
-    },
-     {
-        name: "jack",
-        img: "assets/jack.png"
-    }
-]
 var cardSet;
 var board= [];
 var rows = 4;
