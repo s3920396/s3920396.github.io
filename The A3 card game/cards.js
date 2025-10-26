dbfb77950e76402e33ec3bf2b35913fbbb9a9d85
s3920396 <s3920396@student.rmit.edu.au>
2025-10-26 17:24:49 +1100

    making card array

---
+++ The A3 card game/cards.js
@@ -0,0 +1,26 @@
+const cardArray = [
+    {
+        name: "king",
+        img: "assets/king.png"
+    },
+     {
+        name: "king",
+        img: "assets/king.png"
+    },
+       {
+        name: "queen",
+        img: "assets/queen.png"
+    },
+     {
+        name: "queen",
+        img: "assets/queen.png"
+    },   
+    {
+        name: "jack",
+        img: "assets/jack.png"
+    },
+     {
+        name: "jack",
+        img: "assets/jack.png"
+    }
+]
--- The A3 card game/main.js
+++ The A3 card game/main.js
@@ -1,11 +1,30 @@
 
-var errors = 0;
-var cardList =[
-    "queen",
-    "jack",
-    "king"
+const cardArray = [
+    {
+        name: "king",
+        img: "assets/king.png"
+    },
+     {
+        name: "king",
+        img: "assets/king.png"
+    },
+       {
+        name: "queen",
+        img: "assets/queen.png"
+    },
+     {
+        name: "queen",
+        img: "assets/queen.png"
+    },   
+    {
+        name: "jack",
+        img: "assets/jack.png"
+    },
+     {
+        name: "jack",
+        img: "assets/jack.png"
+    }
 ]
-
 var cardSet;
 var board= [];
 var rows = 4;
