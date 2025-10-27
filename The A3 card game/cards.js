c02a9b2bc59771cfa32bdae250e16f76dc3e8605
s3920396 <s3920396@student.rmit.edu.au>
2025-10-27 13:51:03 +1100

    comenting

--- The A3 card game/cards.js
+++ The A3 card game/cards.js
@@ -1,5 +1,6 @@
 
 // this is a page to store all the cards for the card game. I need to repeat the names twices. Hoever this only gave 6 cards so I duplicated the card names again to get enough cards.
+// This works on a practically level for array. But in future assets I should use figure out some way to simplify it, like how in class excerises we just called the assets folder.
 const cardArray = [
     {
         name: "king",
